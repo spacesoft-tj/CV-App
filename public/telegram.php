6252a5d80202844ecc26d13e1f131d8ca0bdfdde
spacesoft-tj <mirzozoda_s@mail.ru>
2022-07-18 18:55:59 +0500

    deploy

--- public/telegram.php
+++ public/telegram.php
@@ -17,9 +17,5 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: success.html');
-} else {
-  echo "Error";
-}
+
 ?>
